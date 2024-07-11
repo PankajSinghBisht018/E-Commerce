9f71368dbf5fa748841da782c806c18a54b98cc8
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-11 08:31:44 +0530

    searching and filtering

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,12 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../features/CartSlice';
 import themeReducer from '../features/ThemeSlice';
 import authReducer from '../features/AuthSlice';
+import productsReducer from '../features/ProductSlice';
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     theme:themeReducer,
     auth: authReducer,
+    products: productsReducer,
+
 
   },
 });
