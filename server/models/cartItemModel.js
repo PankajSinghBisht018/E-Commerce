63373e828b504f9ecb7a86944472c9d9ec6bbe10
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-31 23:49:57 +0530

    .

---
+++ server/models/cartItemModel.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const cartItemSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  quantity: { type: Number, required: true },
+  price: { type: Number, required: true },
+});
+
+const CartItem = mongoose.model('CartItem', cartItemSchema);
+
+export default CartItem;
